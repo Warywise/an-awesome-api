0bcb7d3062b8cf4fbe723d31936a9c9adef0f17e
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-18 00:23:49 -0300

    Implements all default seeders

--- prisma/seeds/generators/generators.ts
+++ prisma/seeds/generators/generators.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-import { BrProvider, EuroProvider, CreateProducts } from "../interfaces";
-import { BrazilianProvider } from "./BrProvider";
-import { EuropeanProvider } from "./EuroProvider";
+import { BrProvider, EuroProvider, CreateProducts } from '../interfaces';
+import { BrazilianProvider } from './BrProvider';
+import { EuropeanProvider } from './EuroProvider';
 
 const BrProviderPromise: Promise<BrProvider[]> = new Promise((resolve) => setTimeout(
   resolve, 300, BrazilianProvider
@@ -17,19 +17,22 @@ async function getCategories(): Promise<string[]> {
   const categories: string[] = [];
 
   await BrProviderPromise.then((data) => data
-    .forEach(({ categoria }) => !categories.includes(categoria) ?? categories.push(categoria)));
+    .forEach(({ categoria }) => {
+      if (!categories.includes(categoria)) categories.push(categoria);
+    }));
 
-    await EuroProviderPromise.then((data) => data
-      .forEach(({ details: { adjective } }) => !categories
-        .includes(adjective) ?? categories.push(adjective)));
+  await EuroProviderPromise.then((data) => data
+    .forEach(({ details: { adjective } }) => {
+      if (!categories.includes(adjective)) categories.push(adjective);
+    }));
 
   return categories;
 }
 
 export async function generateCategoriesData() {
   const categoriesData = await getCategories();
-  return categoriesData.map((name) => (
-    prisma.category.upsert({
+  return categoriesData.map(async (name) => (
+    await prisma.category.upsert({
       where: { name },
       update: {},
       create: { name }
@@ -53,34 +56,42 @@ async function getProducts() {
         provider: 'Brazilian Provider',
       }
     )
-  ));
-  
+    ));
+
   await EuroProviderPromise.then((data) => data
-  .forEach((product) => products.push(
-    {
-      name: product.name,
-      description: product.description,
-      image: product.gallery,
-      price: Number(product.price),
-      categoryId: categories.findIndex((name) => name === product.details.adjective) + 1,
-      material: product.details.material,
-      provider: 'European Provider',
-      hasDiscount: product.hasDiscount,
-      discountValue: Number(product.discountValue),
-    }
-  )
-  ));
+    .forEach((product) => products.push(
+      {
+        name: product.name,
+        description: product.description,
+        image: product.gallery,
+        price: Number(product.price),
+        categoryId: categories.findIndex((name) => name === product.details.adjective) + 1,
+        material: product.details.material,
+        provider: 'European Provider',
+        hasDiscount: product.hasDiscount,
+        discountValue: Number(product.discountValue),
+      }
+    )
+    ));
 
   return products;
 }
 
 export async function generateProductsData() {
   const productsData = await getProducts();
-  return productsData.map((product) => (
-    prisma.product.upsert({
-      where: { name: product.name },
-      update: {},
-      create: product,
-    })
-  ));
+
+  return productsData.map(async (product) => {
+    const productExists = await prisma.product.findFirst({
+      where: {
+        name: product.name,
+        categoryId: product.categoryId,
+        material: product.material,
+        provider: product.provider,
+      }
+    });
+
+    return productExists
+      ? console.log(`Product already exists: ${product}`)
+      : (prisma.product.create({ data: product, }));
+  });
 }
