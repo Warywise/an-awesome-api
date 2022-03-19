fe300932639145d66e967a9fbb8f5604a3b91483
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-18 21:03:27 -0300

    Implement(product)=> implements endpoint getByCategory

--- src/products/product.router.ts
+++ src/products/product.router.ts
@@ -6,5 +6,6 @@ const ProductRouter = Router();
 
 ProductRouter.get('/products', Product.getAll);
 ProductRouter.get('/products/:id', Product.getById);
+ProductRouter.get('/products/category/:name', Product.getByCategory);
 
 export default ProductRouter;
