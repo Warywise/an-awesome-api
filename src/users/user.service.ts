76a7599b4f0c19ee7893ef82627c254bfd5f11c2
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-21 03:30:16 -0300

    Implement(user)=> Service.getUserInfosByEmail

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -2,11 +2,28 @@ import { PrismaClient } from '@prisma/client';
 import argon from 'argon2';
 import StatusCode from '../utils/enumStatusCodes';
 
+type userToken = { token: string };
+
 class UserService {
   private prisma: PrismaClient;
 
   constructor() {
     this.prisma = new PrismaClient();
+  }
+
+  private async userVerifier(user: userToken | null, token: string) {
+    if (!user) return {
+      code: StatusCode.NOT_FOUND,
+      error: 'User not found'
+    };
+
+    const userAuth = await argon.verify(user.token as string, token);
+
+    return userAuth ? null
+      : {
+        code: StatusCode.UNAUTHORIZED_USER,
+        error: 'Access denied'
+      };
   }
 
   async getUserByEmail(email: string, token: string) {
@@ -22,16 +39,49 @@ class UserService {
       }
     });
 
-    if (!user) return {
+    const userAuth = await this.userVerifier(user as userToken, token);
+
+    return userAuth ?? user;
+  }
+
+  async getUserInfosByEmail(email: string, token: string) {
+    const userInfos = await this.prisma.user.findUnique({
+      where: { email },
+      select: {
+        token: true,
+        userAdress: true,
+        userCard: true,
+        purchases: {
+          select: {
+            payMethod: true,
+            createdAt: true,
+            productsSold: {
+              select: {
+                product: true
+              }
+            }
+          }
+        }
+      }
+    });
+
+    if (!userInfos) return {
       code: StatusCode.NOT_FOUND,
       error: 'User not found'
     };
 
-    const userAuth = await argon.verify(user.token as string, token);
+    const userAuth = await this.userVerifier(userInfos as userToken, token);
 
-    return userAuth
-      ? user
-      : { code: StatusCode.UNAUTHORIZED_USER,
-          error: 'Access denied' };
+    const processUserInfos = {
+      userAdresses: userInfos.userAdress,
+      userCards: userInfos.userCard,
+      userPurchases: userInfos.purchases.map((item) => ({
+        payMethod: item.payMethod,
+        date: item.createdAt,
+        products: item.productsSold
+      })),
+    };
+
+    return userAuth ?? processUserInfos;
   }
 }
