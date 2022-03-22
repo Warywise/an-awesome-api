46dd978be46a58c296ca05d7915a48a1897f38d3
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-22 15:51:16 -0300

    Refac(UserMiddleware)=> refac middlewares to use with @decorators

--- src/middlewares/users.ts
+++ src/middlewares/users.ts
@@ -1,10 +1,11 @@
+import { Middleware } from '@decorators/express';
 import { NextFunction, Request, Response } from 'express';
 import StatusCode from '../utils/enumStatusCodes';
 import { joiEmail, joiHash, joiUserData } from './joiObjects/users';
 import TokenMiddleware from './token';
 
-class UserMiddleware extends TokenMiddleware {
-  async verifyEmail(req: Request, res: Response, next: NextFunction) {
+export class VerifyEmail extends TokenMiddleware implements Middleware{
+  public use(req: Request, res: Response, next: NextFunction) {
     const missingToken = this.verifyToken(req);
     if (missingToken) {
       const { code, error } = missingToken;
@@ -12,27 +13,29 @@ class UserMiddleware extends TokenMiddleware {
     }
 
     const email = req.body;
-    const { error } = joiEmail.validate(email);
+    const { error: err } = joiEmail.validate(email);
 
-    if (error) return res.status(StatusCode.BAD_REQUEST).json({ error });
-    next();
-  }
-
-  async verifyHash(req: Request, res: Response, next: NextFunction) {
-    const hash = req.body;
-    const { error } = joiHash.validate(hash);
-
-    if (error) return res.status(StatusCode.BAD_REQUEST).json({ error });
-    next();
-  }
-
-  async verifyUserData(req: Request, res: Response, next: NextFunction) {
-    const userData = req.body;
-    const { error } = joiUserData.validate(userData);
-
-    if (error) return res.status(StatusCode.BAD_REQUEST).json({ error });
+    if (err) return res.status(StatusCode.BAD_REQUEST).json({ error: err.message });
     next();
   }
 }
 
-export default new UserMiddleware();
+export class VerifyHash implements Middleware {
+  public use(req: Request, res: Response, next: NextFunction) {
+      const hash = req.body;
+      const { error: err } = joiHash.validate(hash);
+  
+      if (err) return res.status(StatusCode.BAD_REQUEST).json({ error: err.message });
+      next();
+    }
+}
+
+export class VerifyUserData implements Middleware {
+  public use(req: Request, res: Response, next: NextFunction) {
+      const userData = req.body;
+    const { error: err } = joiUserData.validate(userData);
+
+    if (err) return res.status(StatusCode.BAD_REQUEST).json({ error: err.message });
+    next();
+  }
+}
