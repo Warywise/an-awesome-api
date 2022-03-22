c804c3125c37cdd77e961c862beb7fce4d6f1956
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-22 16:05:04 -0300

    Implement(user)=> implement endpoint 'getUserInfos'

--- src/users/user.controler.ts
+++ src/users/user.controler.ts
@@ -25,4 +25,22 @@ export default class UserController {
       if (typeof error === 'string') throw Error(error);
     }
   }
+
+  @Get('/infos', [VerifyEmail])
+  async getUserInfos(req: Request, res: Response) {
+    try {
+      const { body: { email }, headers: { authorization } } = req;
+      const userInfos = await UserService.getUserInfosByEmail(email, authorization as string);
+
+      if ('error' in userInfos) {
+        const { code, error } = userInfos;
+        return res.status(code).json({ error });
+      }
+      return res.status(StatusCode.OK).json(userInfos);
+
+    } catch (error) {
+      if (error instanceof Error) throw Error(error.message);
+      if (typeof error === 'string') throw Error(error);
+    }
+  }
 }
