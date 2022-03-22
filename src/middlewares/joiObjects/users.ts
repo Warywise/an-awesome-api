9052501d0306e00ae77e1e6310dd20cbe4d39560
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-22 13:47:33 -0300

    Implement(UserMiddleware)=> implement 'verifyUserData'

--- src/middlewares/joiObjects/users.ts
+++ src/middlewares/joiObjects/users.ts
@@ -3,3 +3,13 @@ import Joi from 'joi';
 export const joiEmail = Joi.object({
   email: Joi.string().email().required(),
 });
+
+export const joiHash = Joi.object({
+  hash: Joi.string().min(6).required(),
+});
+
+export const joiUserData = Joi.object({
+  name: Joi.string().min(2).required(),
+  lastName: Joi.string().min(2).required(),
+  cpf: Joi.string().regex(/^(\d{3}\.){2}\d{3}-\d{2}$/),
+});
--- src/middlewares/users.ts
+++ src/middlewares/users.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import StatusCode from '../utils/enumStatusCodes';
-import { joiEmail } from './joiObjects/users';
+import { joiEmail, joiHash, joiUserData } from './joiObjects/users';
 import TokenMiddleware from './token';
 
 class UserMiddleware extends TokenMiddleware {
@@ -11,11 +11,28 @@ class UserMiddleware extends TokenMiddleware {
       return res.status(code).json({ error });
     }
 
-    const { email } = req.body;
+    const email = req.body;
     const { error } = joiEmail.validate(email);
 
     if (error) return res.status(StatusCode.BAD_REQUEST).json({ error });
+    next();
+  }
 
+  async verifyHash(req: Request, res: Response, next: NextFunction) {
+    const hash = req.body;
+    const { error } = joiHash.validate(hash);
+
+    if (error) return res.status(StatusCode.BAD_REQUEST).json({ error });
+    next();
+  }
+
+  async verifyUserData(req: Request, res: Response, next: NextFunction) {
+    const userData = req.body;
+    const { error } = joiUserData.validate(userData);
+
+    if (error) return res.status(StatusCode.BAD_REQUEST).json({ error });
     next();
   }
 }
+
+export default new UserMiddleware();
