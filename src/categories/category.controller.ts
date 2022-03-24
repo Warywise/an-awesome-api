a2cd0ab954c6822ba00abf5b6561dac6592aeb10
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-24 17:46:58 -0300

    Refac(categories)=> implements new 'TryCatch' method

--- src/categories/category.controller.ts
+++ src/categories/category.controller.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from 'express';
 import { Controller, Get } from '@decorators/express';
 
+import Handler from '../superClass/handler';
 import CategoryService from './category.service';
 import StatusCode from '../utils/enumStatusCodes';
 
 @Controller('/categories')
-export default class CategoryController {
+export default class CategoryController extends Handler {
 
   @Get('')
   async getAll(_req: Request, res: Response) {
-    const allCategories = await CategoryService.getAllCategories();
+    const allCategories = await this
+      .TryCatch(CategoryService.getAllCategories);
 
     return res.status(StatusCode.OK).json(allCategories);
   }
