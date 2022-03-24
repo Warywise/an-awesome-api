fe205f8275df2b2749f5ff9a76b78b65e4a5d293
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-24 18:48:32 -0300

    Refac(middlewares)=> implements an 'index.ts' to export all middlewares

---
+++ src/middlewares/index.ts
@@ -0,0 +1,15 @@
+import {
+  VerifyEmail, VerifyHash, VerifyUserData, VerifyAdressData, VerifyCardData,
+} from './userMiddlewares';
+import { VerifyToken } from './tokenMiddlewares';
+import { VerifySale } from './saleMiddlewares';
+
+export {
+  VerifyEmail,
+  VerifyHash,
+  VerifyCardData,
+  VerifyUserData,
+  VerifyAdressData,
+  VerifyToken,
+  VerifySale,
+};
