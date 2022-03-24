c0665866ec23dc818b68f668e9439f6053a0500b
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-24 18:04:32 -0300

    Refac(UserInfos)=> implements new 'TryCatch' method

--- src/users/userInfos/userInfos.controller.ts
+++ src/users/userInfos/userInfos.controller.ts
@@ -1,53 +1,46 @@
 import { Request, Response } from 'express';
 import { Controller, Post } from '@decorators/express';
 
+import Handler from '../../superClass/handler';
 import UserInfosService from './userInfos.service';
-import StatusCode from '../../utils/enumStatusCodes';
 import { VerifyToken } from '../../middlewares/tokenMiddlewares';
 import { VerifyAdressData, VerifyCardData } from '../../middlewares/userMiddlewares';
+import StatusCode from '../../utils/enumStatusCodes';
 
 @Controller('/users')
-export default class UserInfosController {
+export default class UserInfosController extends Handler {
 
   @Post('/adress/:email', [VerifyToken, VerifyAdressData])
   async createAdress(req: Request, res: Response) {
-    try {
-      const {
-        body: adress, headers: { authorization: token }, params: { email }
-      } = req;
+    const {
+      body: adress, headers: { authorization: token }, params: { email }
+    } = req;
 
-      const userAdress = await UserInfosService.createUserAdress(email, adress, token as string);
+    const userAdress = await this
+      .TryCatch(() => UserInfosService.createUserAdress(email, adress, token as string));
 
-      if ('error' in userAdress) {
-        const { code, error } = userAdress;
-        return res.status(code).json({ error });
-      }
-      return res.status(StatusCode.CREATED).json(userAdress);
-
-    } catch (error) {
-      if (error instanceof Error) throw Error(error.message);
-      if (typeof error === 'string') throw Error(error);
+    if (userAdress && 'error' in userAdress) {
+      const { code, error } = userAdress;
+      return res.status(code).json({ error });
     }
+
+    return res.status(StatusCode.CREATED).json(userAdress);
   }
 
   @Post('/card/:email', [VerifyToken, VerifyCardData])
   async createCard(req: Request, res: Response) {
-    try {
-      const {
-        body: card, headers: { authorization: token }, params: { email }
-      } = req;
+    const {
+      body: card, headers: { authorization: token }, params: { email }
+    } = req;
 
-      const userCard = await UserInfosService.createUserCard(email, card, token as string);
+    const userCard = await this
+      .TryCatch(() => UserInfosService.createUserCard(email, card, token as string));
 
-      if ('error' in userCard) {
-        const { code, error } = userCard;
-        return res.status(code).json({ error });
-      }
-      return res.status(StatusCode.CREATED).json(userCard);
-
-    } catch (error) {
-      if (error instanceof Error) throw Error(error.message);
-      if (typeof error === 'string') throw Error(error);
+    if (userCard && 'error' in userCard) {
+      const { code, error } = userCard;
+      return res.status(code).json({ error });
     }
+
+    return res.status(StatusCode.CREATED).json(userCard);
   }
 }
