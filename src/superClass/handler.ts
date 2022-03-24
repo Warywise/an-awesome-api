4a0db5e6a870fad7dce1ae3032472f8421a6e134
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-24 17:35:49 -0300

    Feature(Handler)=> create a super class 'Handler': - implements method 'TryCatch' to rescue throwed errors

---
+++ src/superClass/handler.ts
@@ -0,0 +1,10 @@
+export default abstract class Handler {
+  protected async TryCatch<T>(serviceFunction: () => Promise<T>) {
+    try {
+      return await serviceFunction();
+    } catch (error) {
+      if (error instanceof Error) throw Error(error.message);
+      if (typeof error === 'string') throw Error(error);
+    }
+  }
+}
