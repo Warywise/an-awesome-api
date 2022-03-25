6d2f68985a7f96f8d8789c628c2bcb9c12a77028
Gustavo Sant'Anna <g_santanna@outlook.com>
2022-03-24 21:21:32 -0300

    Refac(users)=> implements new 'TryCatch' method

--- src/users/user.controler.ts
+++ src/users/user.controler.ts
@@ -1,115 +1,92 @@
 import { Request, Response } from 'express';
 import { Controller, Delete, Get, Post, Put } from '@decorators/express';
 
+import Handler from '../superClass/handler';
 import UserService from './user.service';
 import { VerifyEmail, VerifyHash, VerifyUserData, VerifyToken } from '../middlewares';
 import StatusCode from '../utils/enumStatusCodes';
 
 @Controller('/users')
-export default class UserController {
+export default class UserController extends Handler {
 
   @Get('', [VerifyToken, VerifyEmail])
   async getUser(req: Request, res: Response) {
-    try {
-      const { body: { email }, headers: { authorization } } = req;
-      const user = await UserService.getUserByEmail(email, authorization as string);
+    const { body: { email }, headers: { authorization } } = req;
+    const user = await this
+      .TryCatch(() => UserService.getUserByEmail(email, authorization as string));
 
-      if ('error' in user) {
-        const { code, error } = user;
-        return res.status(code).json({ error });
-      }
-      return res.status(StatusCode.OK).json(user);
-
-    } catch (error) {
-      if (error instanceof Error) throw Error(error.message);
-      if (typeof error === 'string') throw Error(error);
+    if (user && 'error' in user) {
+      const { code, error } = user;
+      return res.status(code).json({ error });
     }
+
+    return res.status(StatusCode.OK).json(user);
   }
 
   @Get('/infos', [VerifyToken, VerifyEmail])
   async getUserInfos(req: Request, res: Response) {
-    try {
-      const { body: { email }, headers: { authorization } } = req;
-      const userInfos = await UserService.getUserInfosByEmail(email, authorization as string);
+    const { body: { email }, headers: { authorization } } = req;
+    const userInfos = await this
+      .TryCatch(() => UserService.getUserInfosByEmail(email, authorization as string));
 
-      if ('error' in userInfos) {
-        const { code, error } = userInfos;
-        return res.status(code).json({ error });
-      }
-      return res.status(StatusCode.OK).json(userInfos);
-
-    } catch (error) {
-      if (error instanceof Error) throw Error(error.message);
-      if (typeof error === 'string') throw Error(error);
+    if (userInfos && 'error' in userInfos) {
+      const { code, error } = userInfos;
+      return res.status(code).json({ error });
     }
+
+    return res.status(StatusCode.OK).json(userInfos);
   }
 
   @Get('/:email')
   async getUserCondition(req: Request, res: Response) {
-    try {
-      const { email } = req.params;
-      const userInfos = await UserService.verifyUserCondition(email);
+    const { email } = req.params;
+    const userInfos = await this
+      .TryCatch(() => UserService.verifyUserCondition(email));
 
-      if ('error' in userInfos) {
-        const { code, error } = userInfos;
-        return res.status(code).json({ error });
-      }
-      return res.status(StatusCode.OK).json(userInfos);
-
-    } catch (error) {
-      if (error instanceof Error) throw Error(error.message);
-      if (typeof error === 'string') throw Error(error);
+    if (userInfos && 'error' in userInfos) {
+      const { code, error } = userInfos;
+      return res.status(code).json({ error });
     }
+
+    return res.status(StatusCode.OK).json(userInfos);
   }
 
   @Post('', [VerifyUserData])
   async upsertUser(req: Request, res: Response) {
-    try {
-      const userData = req.body;
-      const userResult = await UserService.createOrUpdateUser(userData);
+    const userData = req.body;
+    const userResult = await this
+      .TryCatch(() => UserService.createOrUpdateUser(userData));
 
-      return res.status(StatusCode.OK).json(userResult);
-    } catch (error) {
-      if (error instanceof Error) throw Error(error.message);
-      if (typeof error === 'string') throw Error(error);
-    }
+    return res.status(StatusCode.OK).json(userResult);
   }
 
   @Put('', [VerifyToken, VerifyUserData])
   async updateUser(req: Request, res: Response) {
-    try {
-      const { body: userData, headers: { authorization: token } } = req;
-      const userResult = await UserService.updateUser(userData, token as string);
+    const { body: userData, headers: { authorization: token } } = req;
+    const userResult = await this
+      .TryCatch(() => UserService.updateUser(userData, token as string));
 
-      if ('error' in userResult) {
-        const { code, error } = userResult;
-        return res.status(code).json({ error });
-      }
-      return res.status(StatusCode.OK).json(userResult);
-
-    } catch (error) {
-      if (error instanceof Error) throw Error(error.message);
-      if (typeof error === 'string') throw Error(error);
+    if (userResult && 'error' in userResult) {
+      const { code, error } = userResult;
+      return res.status(code).json({ error });
     }
+
+    return res.status(StatusCode.OK).json(userResult);
   }
 
   @Delete('', [VerifyToken, VerifyHash])
   async disableUser(req: Request, res: Response) {
-    try {
-      const {
-        body: { email, hash }, headers: { authorization: token }
-      } = req;
-      const userResult = await UserService.disableUser(email, hash, token as string);
+    const {
+      body: { email, hash }, headers: { authorization: token }
+    } = req;
+    const userResult = await this
+      .TryCatch(() => UserService.disableUser(email, hash, token as string));
 
-      if ('error' in userResult) {
-        const { code, error } = userResult;
-        return res.status(code).json({ error });
-      }
-      return res.status(StatusCode.OK).json(userResult);
-
-    } catch (error) {
-      if (error instanceof Error) throw Error(error.message);
-      if (typeof error === 'string') throw Error(error);
+    if (userResult && 'error' in userResult) {
+      const { code, error } = userResult;
+      return res.status(code).json({ error });
     }
+
+    return res.status(StatusCode.OK).json(userResult);
   }
 }
